1f82fedfdf862997ec31e32fa73a5ab83d40795b
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-19 03:51:13 +0700

    chore: add dto user regiter

---
+++ backend/backend/DTOs/Auth/UserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.DTOs.Auth
+{
+    public class UserRegisterDto
+    {
+        public required string Email { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+    }
+}
