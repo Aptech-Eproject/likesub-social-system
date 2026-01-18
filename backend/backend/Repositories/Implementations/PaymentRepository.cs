effdcfcf431c50a1f38810e57029064068ba496c
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-19 03:55:53 +0700

    feat: implement payment, user repository

---
+++ backend/backend/Repositories/Implementations/PaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using backend.Data;
+using backend.Entities;
+using backend.Repositories.Interfaces;
+
+namespace backend.Repositories.Implementations
+{
+    public class PaymentRepository : GenericRepository<Payment>, IPayment
+    {
+        private readonly AppDbContext _context;
+        public PaymentRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ backend/backend/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using backend.Data;
+using backend.Entities;
+using backend.Repositories.Interfaces;
+
+namespace backend.Repositories.Implementations
+{
+    public class UserRepository : GenericRepository<User>, IUser
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
