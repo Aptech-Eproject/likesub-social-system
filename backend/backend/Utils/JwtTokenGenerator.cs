0d7e8d2d250eb2463761afdd06f29ee0c327d605
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-19 03:39:26 +0700

    chore: add util jwt generate token

---
+++ backend/backend/Utils/JwtTokenGenerator.cs
@@ -0,0 +1,39 @@
+﻿namespace backend.Utils
+{
+    public class JwtTokenGenerator
+    {
+        private readonly IConfiguration _config;
+        public JwtTokenGenerator(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public AuthResponseDto GenerateAccessToken(User user)
+        {
+            var jwt = _config.GetSection("JWT");
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expires = DateTime.UtcNow.AddMinutes(double.Parse(jwt["ExpireMinutes"]!));
+            var token = new JwtSecurityToken(
+                issuer: jwt["Issuer"],
+                audience: jwt["Audience"],
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new AuthResponseDto
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(token),
+            };
+        }
+    }
+}
